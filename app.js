5cbd172e7e0e32df3951f77aee3a0fdf6e938cfc
Sevil Rasulova <sevillerasulova@gmail.com>
2020-11-05 22:45:30 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,64 +1,108 @@
-const form = document.getElementById('form');
 const fname = document.getElementById('fname');
 const lname = document.getElementById('lname');
 const birthday = document.getElementById('birthday');
 const email = document.getElementById('email');
 const number = document.getElementById('number');
 const subject = document.getElementById('subject');
+const btn = document.getElementById('btn')
+const modalDiv = document.getElementById('modal-close-default');
 
-form.addEventListener('submit', e => {
+btn.addEventListener('click', function(e) {
 	e.preventDefault();
 	
 	checkInputs();
 });
 
 function checkInputs() {
-	// trim to remove the whitespaces
-    const fnameValue = fname.value.trim();
-	const lnameValue = lname.value.trim();
-	const birthdayValue = birthday.value.trim();
-    const emailValue = email.value.trim();
-    const numberValue = number.value.trim();
-    const subjectValue = subject.value;
-    
+    const fnameValue = fname.value;
+	const lnameValue = lname.value;
+	const birthdayValue = birthday.value;
+    const emailValue = email.value;
+    const numberValue = number.value;
+	const subjectValue = subject.value;
+	var check1 = false;
+	var check2 = false;
+	var check3 = false;
+	var check4 = false;
+	var check5 = false;
+	var check6 = false;
+
     if(fnameValue === '') {
 		setErrorFor(fname, 'First Name cannot be blank');
+		check1 = false;
 	} else {
 		setSuccessFor(fname);
+		check1 = true;
     }
     
     if(lnameValue === '') {
 		setErrorFor(lname, 'Last Name cannot be blank');
+		check2 = false;
 	} else {
 		setSuccessFor(lname);
+		check2 = true;
     }
 
 	if(birthdayValue === '') {
 		setErrorFor(birthday, 'Birthday cannot be blank');
+		check3 = false;
 	} else {
 		setSuccessFor(birthday);
+		check3 = true;
     }
     
 	if(emailValue === '') {
 		setErrorFor(email, 'Email cannot be blank');
+		check4 = false;
 	} else if (!isEmail(emailValue)) {
 		setErrorFor(email, 'Not a valid email');
+		check4 = false;
 	} else {
 		setSuccessFor(email);
+		check4 = true;
 	}
 	
 	if(numberValue === '') {
 		setErrorFor(number, 'Phone Number cannot be blank');
+		check5 = false;
 	} else {
 		setSuccessFor(number);
+		check5 = true;
     }
     
     if(subjectValue === '') {
 		setErrorFor(subject, 'Subject cannot be blank');
+		check6 = false;
 	} else {
 		setSuccessFor(subject);
+		check6 = true;
     }
-    console.log(lname, fname)
+	 
+	if (check1 && check2 && check3 && check4 && check5 && check6){
+		btn.setAttribute("uk-toggle", "target: #modal-close-default");
+		
+		modal = document.querySelector(".uk-modal-body")
+
+		var data1 = document.createElement("p")
+		data1.innerHTML = `Your First Name: ${fnameValue}`;
+
+		var data2 = document.createElement("p")
+		data2.innerHTML = `Your Last Name: ${lnameValue}`;
+
+		var data3 = document.createElement("p")
+		data3.innerHTML = `Your Email: ${emailValue}`;
+
+		var data4 = document.createElement("p")
+		data4.innerHTML = `Your Subject: ${subjectValue}`;
+
+		modal.appendChild(data1);
+		modal.appendChild(data2);
+		modal.appendChild(data3);
+		modal.appendChild(data4);
+	}
+	else{
+		btn.removeAttribute("uk-toggle");
+	}
 }
 
 function setErrorFor(input, message) {
@@ -74,9 +118,9 @@ function setSuccessFor(input) {
 }
 	
 function isEmail(email) {
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+	if (email.includes("@")){
+		return true;
+	}else{
+		return false;
+	}
 }
-
-
-
-
